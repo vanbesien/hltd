c0df946dcdbf2c91b4f95e5a7ac7bc654d5bb3bf
zazasa <salvatore.zaza@gmail.com>
2014-10-31 15:54:15 +0100

    rpm updated

--- scripts/hltdrpm.sh
+++ scripts/hltdrpm.sh
@@ -156,8 +156,8 @@ cd $TOPDIR
 # we are done here, write the specs and make the fu***** rpm
 cat > hltd.spec <<EOF
 Name: hltd
-Version: 1.5.1
-Release: 5
+Version: 1.5.3
+Release: 0
 Summary: hlt daemon
 License: gpl
 Group: DAQ
